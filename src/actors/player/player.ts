ee5bbcd5aa9159011e74d61b94ee3bd8eccf01ae
Ian Hills <v@bitvox.me>
2018-09-20 23:12:10 -0400

    first cow commit

--- src/actors/player/player.ts
+++ src/actors/player/player.ts
@@ -1,6 +1,7 @@
 import * as ex from 'excalibur';
 
 export class Player extends ex.Actor {
+  speed: number;
   constructor() {
     super();
     this.setWidth(25);
@@ -8,5 +9,30 @@ export class Player extends ex.Actor {
     this.x = 150;
     this.y = 150;
     this.color = new ex.Color(255, 255, 255);
+    this.speed = 0.1;
+  }
+
+  public update(engine: ex.Engine, delta: number) {
+    if (engine.input.keyboard.wasPressed(ex.Input.Keys.Shift)) {
+      this.speed = 0.5;
+    }
+    if (engine.input.keyboard.wasReleased(ex.Input.Keys.Shift)) {
+      this.speed = 0.1;
+    }
+
+
+    super.update(engine, delta);
+    if (engine.input.keyboard.isHeld(ex.Input.Keys.W)) {
+      this.y = delta * -this.speed + this.y;
+    }
+    if (engine.input.keyboard.isHeld(ex.Input.Keys.S)) {
+      this.y = delta * this.speed + this.y;
+    }
+    if (engine.input.keyboard.isHeld(ex.Input.Keys.A)) {
+      this.x = delta * -this.speed + this.x;
+    }
+    if (engine.input.keyboard.isHeld(ex.Input.Keys.D)) {
+      this.x = delta * this.speed + this.x;
+    }
   }
 }
--- src/scenes/level-one/level-one.ts
+++ src/scenes/level-one/level-one.ts
@@ -1,7 +1,7 @@
 import * as ex from 'excalibur';
 
 export class LevelOne extends ex.Scene {
-  public onInitialize(engine: ex.Engine) {}
-  public onActivate() {}
-  public onDeactivate() {}
+  public onInitialize(engine: ex.Engine) { }
+  public onActivate() { }
+  public onDeactivate() { }
 }
